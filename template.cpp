4143a557eadc02503e20e012078661702a9183c7
Insee <insee.t@kkumail.com>
2025-07-17 02:17:38 +0700

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define int long long
+#define ll long long
+#define sz(x) (int)(x).size()
+
+using namespace std;
+
+void sol() {
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    sol();
+}
